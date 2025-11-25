a23d34f62006fb5a0b8b4b17a00a93bd5e1ec46a
Your hrbrhr <helenbiryukova@gmail.com>
2025-11-25 17:24:55 +0300

    Connect jUnit and create test

---
+++ src/test/java/app/core/ClassMyFirstTest.java
@@ -0,0 +1,14 @@
+package app.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClassMyFirstTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
