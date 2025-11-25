3002e0477d9aa4aada490d5ec4c3a023d764d7dc
Your hrbrhr <helenbiryukova@gmail.com>
2025-11-25 17:33:25 +0300

    Create test 2.0

--- src/test/java/app/core/MyFirstClassTest.java
+++ src/test/java/app/core/MyFirstClassTest.java
@@ -3,12 +3,10 @@ package app.core;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-class ClassMyFirstTest {
+class MyFirstClassTest {
 
     @Test
     public void test() {
         assertTrue(true);
     }
-
 }
